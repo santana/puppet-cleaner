5fc1fd255fcf315384464fe51660890c7cbeb53e
Ben Ford <ben.ford@puppetlabs.com>
2014-06-05 13:22:59 -0700

    initialize puppet settings

--- lib/puppet-cleaner/inspect.rb
+++ lib/puppet-cleaner/inspect.rb
@@ -2,7 +2,9 @@ require 'puppet'
 
 module Puppet::Cleaner
   def self.inspect(filename)
-    Puppet[:manifest] = filename
+    Puppet.initialize_settings
+
+    Puppet.settings[:manifest] = filename
     tc = Puppet::Node::Environment.new(Puppet[:environment]).known_resource_types
     catalog = [:nodes, :hostclasses, :definitions].collect {|meth| tc.send(meth) }
     tc.clear
